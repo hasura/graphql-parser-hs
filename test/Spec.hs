79aee206bf14a675fb50ee94883e2dc999dcedde
Anon Ray <rayanon004@gmail.com>
2019-03-13 10:57:17 +0530

    initial version of pretty printer

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,22 @@
-import Protolude
+import           Protolude
+
+import           Language.GraphQL.Draft.Parser (parseExecutableDoc)
+import           Language.GraphQL.Draft.Print
+
+import qualified Data.Text.IO                  as T
+
 
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
+
+testFile :: FilePath -> IO ()
+testFile fp = do
+  contents <- T.readFile fp
+  test contents
+
+test :: Text -> IO ()
+test str = case parseExecutableDoc str of
+  Left err  -> print err
+  Right doc -> do
+    T.putStrLn $ renderPretty $ executableDocument doc
+    T.putStrLn $ renderCompact $ executableDocument doc
