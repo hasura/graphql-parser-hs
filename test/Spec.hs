0d5eb50fe1085c0efeef7a75bba126d241b8e6b8
Anon Ray <rayanon004@gmail.com>
2019-05-02 12:39:23 +0530

    refactor test code

--- test/Spec.hs
+++ test/Spec.hs
@@ -12,6 +12,7 @@ import qualified Data.Text.Lazy.Encoding                   as TL
 
 import           Language.GraphQL.Draft.Generator.Document
 import           Language.GraphQL.Draft.Parser             (parseExecutableDoc)
+import           Language.GraphQL.Draft.Syntax
 
 import qualified Language.GraphQL.Draft.Printer.ByteString as PP.BB
 import qualified Language.GraphQL.Draft.Printer.LazyText   as PP.TLB
@@ -42,47 +43,32 @@ runTest = void . tests
 tests :: TestLimit -> IO Bool
 tests nTests =
   checkParallel $ Group "Test.printer.parser"
-    [ ("property [ parse (prettyPrint ast) == ast ]", propParserPrinter nTests)
+    [ ("property [ parse (prettyPrint ast) == ast ]", propParserPrettyPrinter nTests)
     , ("property [ parse (textBuilderPrint ast) == ast ]", propParserTextPrinter nTests)
     , ("property [ parse (lazyTextBuilderPrint ast) == ast ]", propParserLazyTextPrinter nTests)
     , ("property [ parse (bytestringBuilderPrint ast) == ast ]", propParserBSPrinter nTests)
     ]
 
-propParserPrinter :: TestLimit -> Property
-propParserPrinter space =
-  withTests space $ property $ do
-    xs <- forAll genExecutableDocument
-    let rendered = PP.renderExecutableDoc xs
-    either (\e -> print e >> fail e) (xs ===) $ parseExecutableDoc rendered
-  where
-    fail e = footnote (T.unpack e) >> failure
+propParserPrettyPrinter :: TestLimit -> Property
+propParserPrettyPrinter = mkPropParserPrinter PP.renderExecutableDoc
 
 propParserTextPrinter :: TestLimit -> Property
-propParserTextPrinter space =
-  withTests space $ property $ do
-    xs <- forAll genExecutableDocument
-    let rendered = PP.TB.renderExecutableDoc xs
-    either (\e -> print e >> fail e) (xs ===) $ parseExecutableDoc rendered
-  where
-    fail e = footnote (T.unpack e) >> failure
+propParserTextPrinter = mkPropParserPrinter PP.TB.renderExecutableDoc
 
 propParserLazyTextPrinter :: TestLimit -> Property
-propParserLazyTextPrinter space =
-  withTests space $ property $ do
-    xs <- forAll genExecutableDocument
-    let rendered = PP.TLB.renderExecutableDoc xs
-    either (\e -> print e >> fail e) (xs ===) $ parseExecutableDoc $
-      TL.toStrict rendered
-  where
-    fail e = footnote (T.unpack e) >> failure
+propParserLazyTextPrinter =
+  mkPropParserPrinter (TL.toStrict . PP.TLB.renderExecutableDoc)
 
 propParserBSPrinter :: TestLimit -> Property
-propParserBSPrinter space =
+propParserBSPrinter =
+  mkPropParserPrinter (bsToTxt . PP.BB.renderExecutableDoc)
+
+mkPropParserPrinter :: (ExecutableDocument -> Text) -> (TestLimit -> Property)
+mkPropParserPrinter printer = \space ->
   withTests space $ property $ do
     xs <- forAll genExecutableDocument
-    let rendered = PP.BB.renderExecutableDoc xs
-    either (\e -> print e >> fail e) (xs ===) $ parseExecutableDoc $
-      bsToTxt rendered
+    let rendered = printer xs
+    either (\e -> print e >> fail e) (xs ===) $ parseExecutableDoc rendered
   where
     fail e = footnote (T.unpack e) >> failure
 
