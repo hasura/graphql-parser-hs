241565f9c7e6f0f68b8563def47a0e26bd5dbee2
jkachmar <git@jkachmar.com>
2022-04-11 09:15:04 -0400

    Addresses CI failures

--- test/Spec.hs
+++ test/Spec.hs
@@ -40,10 +40,12 @@ import Prettyprinter.Render.Text qualified as PP
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
 import Text.Builder qualified as TB
+import Data.Kind (Type)
 import Prelude
 
 -------------------------------------------------------------------------------
 
+type TestMode :: Type
 data TestMode = TMDev | TMQuick | TMRelease
   deriving stock (Show)
 
