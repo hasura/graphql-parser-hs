f823742658ec3bdde72a66e2e761f11efad61c5f
Tom Harding <tomjharding@live.co.uk>
2022-05-11 13:34:03 +0100

    Begin to convert keywords tests
    More work on porting the test suite across to Tasty. Most of these are
    nice, straightforward unit tests, though I definitely think we should
    write some property tests soon. There are some really nice candidates in
    this library.

--- tests/Language/GraphQL/Draft/ParserTest.hs
+++ tests/Language/GraphQL/Draft/ParserTest.hs
@@ -1,18 +1,23 @@
 {-# OPTIONS_GHC -fno-warn-deprecations #-}
 
+{-# LANGUAGE TemplateHaskell #-}
+
 module Language.GraphQL.Draft.ParserTest where
 
 -------------------------------------------------------------------------------
 
 import Data.Text (Text)
 import Data.Text qualified as Text
+import Data.Void (Void)
 import Hedgehog (Property, failure, footnote, forAll, property, (===))
 import Hedgehog.Gen qualified as Gen
 import Hedgehog.Range qualified as Range
+import Language.GraphQL.Draft.Parser (Parser, blockString, nameParser, runParser, value)
+import Language.GraphQL.Draft.Printer qualified as Printer
+import Language.GraphQL.Draft.Syntax (EnumValue (..), Name, Value (..), litName)
 import Prelude
--- import Text.RawString.QQ (r)
 import Test.Hspec (Expectation, Spec, describe, it, shouldBe)
-import Language.GraphQL.Draft.Parser (blockString, runParser)
+import Text.Builder (Builder, run)
 
 -------------------------------------------------------------------------------
 
@@ -59,8 +64,8 @@ spec_whitespace_blockstring = do
   it "does not preserve subsequent whitespace if the first character is a newline" do
     "\n hey  " `shouldParseTo` "hey  "
 
-spec_unit_tests :: Spec
-spec_unit_tests = do
+spec_blockquotes :: Spec
+spec_blockquotes = do
   it "parses the model \"Hello, World\" example" do
     let input  = "\n    Hello,\n      World!\n\n    Yours,\n      GraphQL.\n  "
         output = "Hello,\n  World!\n\nYours,\n  GraphQL."
@@ -70,7 +75,7 @@ spec_unit_tests = do
   it "parses non-whitespace characters" do
     "x" `shouldParseTo` "x"
 
-  -- XXX(i-am-tom)
+  -- XXX(i-am-tom): is this... good?
   it "doesn't remove non-whitespace escaped characters" do
     "  \NULL  " `shouldParseTo` "  \NULL  "
 
@@ -89,8 +94,90 @@ spec_unit_tests = do
   it "escaped triple-quotes are ignored as block terminator" do
     "\n   \\\"\"\"hey\n   friends\n" `shouldParseTo` "\"\"\"hey\nfriends"
 
+spec_keywords :: Spec
+spec_keywords = do
+  it "parses names that begin with null" do
+    let test :: Name
+        test = $$(litName "nullColumn")
+
+    parseAndPrint nameParser Printer.nameP test `shouldBe` Right test
+
+  it "parses enum names that begin with null" do
+    let test :: Value Void
+        test = VList [VEnum (EnumValue $$(litName "nullColumn"))]
+
+    parseAndPrint value Printer.value test `shouldBe` Right test
+
+  it "parses enum names that begin with true" do
+    let test :: Value Void
+        test = VList [VEnum (EnumValue $$(litName "trueColumn"))]
+
+    parseAndPrint value Printer.value test `shouldBe` Right test
+
+--   it "a string containing \\NUL is handled correctly" do
+--     withTests 1 propHandleNulString
+--
+--   it "a string containing \\n is handled correctly" do
+--     withTests 1 propHandleNewlineString
+--
+--   it "a string containing \\x0011 is handled correctly" do
+--     withTests 1 propHandleControlString
+--
+--   it "all unicode characters are supported" do
+--     withTests 1 propHandleUnicodeCharacters
+--
+--   it "triple quotes is a valid string" do
+--     withTests 1 propHandleTripleQuote
+--
+--   it "name with a suffix should be a valid name" do
+--     withTests 1 propNameWithSuffix
+--
+-- propNullNameName :: Property
+-- propNullNameName =
+--   property $
+--     roundtripParser nameParser Printer.nameP $$(litName "nullColumntwo")
+--
+-- propHandleNulString :: Property
+-- propHandleNulString = property . roundtripValue $ VString "\NUL"
+--
+-- propHandleNewlineString :: Property
+-- propHandleNewlineString = property . roundtripValue $ VString "\n"
+--
+-- propHandleControlString :: Property
+-- propHandleControlString = property . roundtripValue $ VString "\x0011"
+--
+-- -- NB: 'liftTest' is explicitly used to restrict the 'for_' block to operate in
+-- -- the 'Test' type (i.e. 'type Test = TestT Identity'), as opposed to 'PropertyT
+-- -- IO'.  The 'Test' monad is a thinner monad stack & therefore doesn't suffer
+-- -- from memory leakage caused by, among others, Hedgehog's 'TreeT', which is
+-- -- used for automatic shrinking (which we don't need in this test).
+-- propHandleUnicodeCharacters :: Property
+-- propHandleUnicodeCharacters = property . liftTest $
+--   for_ [minBound .. maxBound] $ \char ->
+--     roundtripValue . VString $ singleton char
+--
+-- propHandleTripleQuote :: Property
+-- propHandleTripleQuote = property . roundtripValue $ VString "\"\"\""
+--
+-- propNameWithSuffix :: Property
+-- propNameWithSuffix =
+--   property . roundtripValue $
+--     VList [VEnum $ EnumValue (addSuffixes $$(litName "prefix") [$$(litSuffix "1suffix"), $$(litSuffix "2suffix")])]
+--
+-- -- | Test that a given 'Value'@ @'Void' passes round-trip tests as expected.
+-- roundtripValue :: (MonadTest m) => Value Void -> m ()
+-- roundtripValue = roundtripParser value Printer.value
+
+-- | Given a parser, printer, and text builder, attempt to round-trip a piece
+-- of AST. In other words, try to parse a printed value back into the original
+-- value. This is used in the spec_keywords tests.
+parseAndPrint :: (Eq a, Show a) => Parser a -> (a -> Builder) -> a -> Either Text a
+parseAndPrint parser printer = runParser parser . run . printer
+
 -------------------------------------------------------------------------------
 
+-- | Check that empty blockstrings (i.e. just newlines) always parse to empty
+-- strings.
 hprop_empty_lines :: Property
 hprop_empty_lines = property do
   n <- forAll $ Gen.int (Range.linear 0 100)
@@ -104,12 +191,15 @@ hprop_empty_lines = property do
 
 -------------------------------------------------------------------------------
 
+-- | Assert that the blockstring text on the left should be parsed into the
+-- text on the right.
 shouldParseTo :: Text -> Text -> Expectation
 shouldParseTo unparsed expected = do
   case runParser blockString ("\"\"\"" <> unparsed <> "\"\"\"") of
     Right r -> r `shouldBe` expected
     Left  l -> fail (Text.unpack l)
 
+-- | Assert that the given blockstring text should fail to parse.
 parseFailure :: Text -> Expectation
 parseFailure unparsed = do
   case runParser blockString ("\"\"\"" <> unparsed <> "\"\"\"") of
